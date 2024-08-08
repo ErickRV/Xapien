f2234652c9a7f665ebc8638627426e3b5fec3319
Z3LT1K09 <erickromanvazquez@hotmail.com>
2024-08-07 17:36:04 -0700

    [DEV] Creando ejemplo basico

---
+++ Examples/Basic/Xapien.Example.Basic/Program.cs
@@ -0,0 +1,56 @@
+﻿using Xapien.Core;
+using Xapien.Entities;
+
+namespace Xapien.Example.Basic
+{
+    internal class Program
+    {
+        //This is the Orchestrator class
+        static async Task Main(string[] args)
+        {
+            XapienBuilder builder = new XapienBuilder();
+            builder.AddXThread("Hilo 1", 
+                new List<Step> 
+                { 
+                    new Step("C:\\Xapien Show\\Programs\\FileManager\\Example.FileWriter.exe",
+                    "WriteFile -Path \"C:\\Xapien Show\\XThread\\\\\" -Name \"Hola\"",
+                    ""),
+
+                    new Step("C:\\Xapien Show\\Programs\\FileManager\\Example.FileWriter.exe",
+                    "WriteFile -Path \"C:\\Xapien Show\\XThread\\\\\" -Name \"Mundo\"",
+                    ""),
+
+                    new Step("C:\\Xapien Show\\Programs\\FileManager\\Example.FileWriter.exe",
+                    "KillFile -Path \"C:\\Xapien Show\\XThread\\Hola.txt\"",
+                    ""),
+
+                    new Step("C:\\Xapien Show\\Programs\\FileManager\\Example.FileWriter.exe",
+                    "KillFile -Path \"C:\\Xapien Show\\XThread\\Mundo.txt\"",
+                    ""),
+                });
+
+            builder.AddXThread("Hilo 2",
+                new List<Step>
+                {
+                    new Step("C:\\Xapien Show\\Programs\\FileManager\\Example.FileWriter.exe",
+                    "WriteFile -Path \"C:\\Xapien Show\\YThread\\\\\" -Name \"Hola\"",
+                    ""),
+
+                    new Step("C:\\Xapien Show\\Programs\\FileManager\\Example.FileWriter.exe",
+                    "WriteFile -Path \"C:\\Xapien Show\\YThread\\\\\" -Name \"Mundo\"",
+                    ""),
+
+                    new Step("C:\\Xapien Show\\Programs\\FileManager\\Example.FileWriter.exe",
+                    "KillFile -Path \"C:\\Xapien Show\\YThread\\Hola.txt\"",
+                    ""),
+
+                    new Step("C:\\Xapien Show\\Programs\\FileManager\\Example.FileWriter.exe",
+                    "KillFile -Path \"C:\\Xapien Show\\YThread\\Mundo.txt\"",
+                    ""),
+                });
+
+            Xapien.Core.Xapien xapien = builder.Build();
+            await xapien.Run();
+        }
+    }
+}
